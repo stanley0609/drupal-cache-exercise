770d567ec19412c0bcaaa5655c02bf3be94a34ea
Stanley Fernandes <stanleyff06@gmail.com>
2025-03-23 16:32:43 +0530

    Memecache and Purger implementation in Drupal.

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -878,3 +878,8 @@ if (getenv('IS_DDEV_PROJECT') == 'true' && file_exists(__DIR__ . '/settings.ddev
 # if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
 #   include $app_root . '/' . $site_path . '/settings.local.php';
 # }
+
+$settings['memcache']['servers'] = ['127.0.0.1:11211' => 'default'];
+$settings['memcache']['bins'] = ['default' => 'default'];
+$settings['memcache']['key_prefix'] = '';
+$settings['cache']['default'] = 'cache.backend.memcache';
